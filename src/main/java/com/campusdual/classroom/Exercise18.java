56204a507d09ecf0e303ffd5817c89adef5245b0
hiraryw64 <hiraryw64@gmail.com>
2024-09-26 14:54:58 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,24 @@
 package com.campusdual.classroom;
 
 public class Exercise18 {
+    public static void main(String[] args) {
+        showInlineArray(createAndInitializeArray(4));
+    }
 
-	public static void main(String[] args) {
+    public static int[] createAndInitializeArray(int num) {
+        int[] array = new int[num];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
 
-	}
+    public static void showInlineArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("numero" + array[i]);
+            if (i < array.length - 1) {
+                System.out.println(" ");
+            }
+        }
+    }
 }
